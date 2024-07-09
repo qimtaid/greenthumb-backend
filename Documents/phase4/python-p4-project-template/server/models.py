723fb33b0bff80912479091fbbffad55d794c671
Dan Koskei <qimtaid@gmail.com>
2024-07-09 03:54:46 +0300

    Complete lab

---
+++ Documents/phase4/python-p4-project-template/server/models.py
@@ -0,0 +1,7 @@
+from config import db
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    date = db.Column(db.String(20), nullable=False)
